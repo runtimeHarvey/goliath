6b7cc193d9c46ce83958d526ac018dd37390e71f
asyncHarvey <harveyn.uk@gmail.com>
2026-07-24 00:00:01 +0100

    Auto update 2026-07-24 00:00:01

--- src/main/java/net/dellsmp/goliath/core/managers/DevModeManager.java
+++ src/main/java/net/dellsmp/goliath/core/managers/DevModeManager.java
@@ -1,4 +1,22 @@
 package net.dellsmp.goliath.core.managers;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 public class DevModeManager {
+
+    private final Map<UUID, Long> devModes = new HashMap<>();
+
+    public void toggle(UUID uuid){
+        if(devModes.containsKey(uuid)){
+            devModes.remove(uuid);
+            return;
+        }
+        devModes.put(uuid, System.currentTimeMillis());
+    }
+
+    public void clear(){
+        devModes.clear();
+    }
 }
